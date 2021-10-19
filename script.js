02bac843e372ae57d9fe661ba41354b50004e6b9
Isabela Oliveira <oliver.isabela2303@gmail.com>
2021-10-19 19:06:47 -0300

    checking tasks function fix

--- script.js
+++ script.js
@@ -24,13 +24,57 @@ function postTask() {
         <button class="close"></button>
         `
         taskList.append(newTask);
-        itemLeft.innerText = `${taskList.children.length} items left`;
+        itemLeft.innerText = `${taskList.children.length} item(s) left`;
     }
 
     tasks = [].slice.call(taskList.querySelectorAll('.task'), 0);
 }
 
+let all = document.getElementById('all'),
+    active = document.getElementById('active'),
+    completed = document.getElementById('completed');
+
+function sort() {
+    let completedTasks = document.querySelectorAll('li.task.completo'),
+        activeTasks = document.querySelectorAll('li.task:not(.completo)');
+
+    if (active.classList.contains('sorted')) {
+        completedTasks.forEach(e => e.style.display = 'none');
+        activeTasks.forEach(e => e.style.display = 'flex');
+        itemLeft.innerText = `${activeTasks.length} active tasks`;
+    } else if (completed.classList.contains('sorted')) {
+        activeTasks.forEach(e => e.style.display = 'none');
+        completedTasks.forEach(e => e.style.display = 'flex');
+        itemLeft.innerText = `${completedTasks.length} completed tasks`;
+    } else {
+        activeTasks.forEach(e => e.style.display = 'flex');
+        completedTasks.forEach(e => e.style.display = 'flex');
+        itemLeft.innerText = `${taskList.children.length} item(s) left`;
+    }
+}
+
+[all, active, completed].forEach(e => {
+    e.addEventListener('click', evt => {
+        [all, active, completed].forEach(e => e.classList.remove('sorted'));
+
+        if (evt.currentTarget == active) {
+            active.classList.add('sorted');
+
+        } else if (evt.currentTarget == completed) {
+            completed.classList.add('sorted');
+
+        } else {
+            all.classList.add('sorted');
+        }
+
+        sort();
+    })
+})
+
 post.addEventListener('click', () => {
+    [all, active, completed].forEach(e => e.classList.remove('sorted'));
+    all.classList.add('sorted');
+    sort();
     postTask();
     // create.value = '';
 })
@@ -41,21 +85,47 @@ window.addEventListener('keyup', evt => {
     }
 })
 
-taskList.addEventListener('click', evt => {
-    let index = tasks.indexOf(evt.target);
+let index;
+function completeTask() {
     if (index !== -1) {
         tasks[index].classList.add('completo')
         tasks[index].querySelector('.checa').classList.add('completo');
-        
         let completas = taskList.querySelectorAll('li.task.completo');
-        itemLeft.innerText = `${taskList.children.length - completas.length} items left`;
-    } else if(evt.target.matches('.close')) {
-        let item = evt.target.parentElement;
-        // console.log(item);
-        taskList.removeChild(item)
-        itemLeft.innerText = `${taskList.children.length} items left`;
+        itemLeft.innerText = `${taskList.children.length - completas.length} item(s) left`;
+
+    }
+}
+
+taskList.addEventListener('click', evt => {
+    if (evt.target.nodeName == 'P' || evt.target.nodeName == 'DIV' || evt.target.matches('.checa')) {
+        index = tasks.indexOf(evt.target.closest('li'));
+        completeTask();
+        if (evt.target.matches('.close')) {
+            let item = evt.target.parentElement;
+            taskList.removeChild(item)
+            itemLeft.innerText = `${taskList.children.length} item(s) left`;
+        }
+        sort();
+    } else {
+        index = tasks.indexOf(evt.target);
+        completeTask();
+        if (evt.target.matches('.close')) {
+            let item = evt.target.parentElement;
+            taskList.removeChild(item)
+            itemLeft.innerText = `${taskList.children.length} item(s) left`;
+        }
+        sort();
     }
 });
+
+let clearCompleted = document.getElementById('clear');
+clearCompleted.addEventListener('click', () => {
+    let completedTasks = document.querySelectorAll('li.task.completo');
+    completedTasks.forEach(e => taskList.removeChild(e));
+    itemLeft.innerText = `${taskList.children.length} item(s) left`;
+})
+
+
 
 
 

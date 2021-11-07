8da1bc4c1723e6b470572b7e531eebd33b4a916e
Isabela Oliveira <oliver.isabela2303@gmail.com>
2021-11-07 18:42:22 -0300

    adding a manifest and a install button

--- js/install.js
+++ js/install.js
@@ -1,5 +1,34 @@
-if('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js')
-    .then(reg => console.log('registrado', reg))
-    .catch(err => console.log('erro', err))
-}
+const installBtn = document.getElementById("install");
+window.addEventListener('DOMContentLoaded', () => {
+    installBtn.style.display = 'none';
+
+    if ('serviceWorker' in navigator) {
+        installBtn.style.display = 'block';
+        navigator.serviceWorker.register('sw.js')
+            .then(reg => console.log('registrado', reg))
+            .catch(err => console.log('erro', err))
+    }
+
+    if (window.matchMedia('(display-mode: standalone)').matches) {
+        installBtn.style.display = 'none';
+      }
+})
+
+let deferredPrompt;
+window.addEventListener('beforeinstallprompt', e => {
+    deferredPrompt = e;
+});
+
+installBtn.addEventListener('click', async () => {
+    if (deferredPrompt !== null) {
+        deferredPrompt.prompt();
+
+        const { outcome } = await deferredPrompt.userChoice;
+        if (outcome === 'accepted') {
+            deferredPrompt = null;
+            installBtn.style.display = 'none';
+        }
+    }
+});
+
+
--- sw.js
+++ sw.js
@@ -1,10 +1,15 @@
-const staticCache = 'static-03';
+const staticCache = 'static-00',
+    dynamicCache = 'dynamic-00';
 
 const Resources = async () => {
     let assets = [
         './',
         './index.html',
+        'js/min/app.min.js',
+        'js/min/loader.min.js',
+        'js/min/main.min.js',
         'js/script.js',
+        'js/install.js',
         'css/style.min.css',
         'img/icon-cross.svg',
         'img/icon-moon.svg',
@@ -15,7 +20,6 @@ const Resources = async () => {
     return cache.addAll(assets);
 }
 
-
 self.addEventListener('install', e => {
     e.waitUntil(Resources())
 });
@@ -23,7 +27,7 @@ self.addEventListener('install', e => {
 const updateCache = async () => {
     let key = await caches.keys();
     return Promise.all(
-        key.filter(chave => chave !== staticCache).map(chave => caches.delete(chave))
+        key.filter(chave => chave !== staticCache && chave !== dynamicCache).map(chave => caches.delete(chave))
     )
 }
 
@@ -35,8 +39,16 @@ self.addEventListener('fetch', e => {
     e.respondWith(
         caches.match(e.request).then(cacheResponse => {
             return cacheResponse || fetch(e.request)
+                .then(fetchResponse => {
+                    return caches.open(dynamicCache)
+                        .then(cache => {
+                            cache.put(e.request.url, fetchResponse.clone());
+                            return fetchResponse;
+                        })
+                });
         })
-    ).catch(erro => {
-        console.log(erro)
-    })
-})
+            .catch(erro => {
+                console.log(erro)
+            })
+    );
+});
